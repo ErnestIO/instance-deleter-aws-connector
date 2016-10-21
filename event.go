537532e2c9e3c6285c4b6e2d2b37197d90a17db1
Tom Bevan <tom.bevan@r3labs.io>
2016-10-21 15:14:46 +0100

    correcting vpc credentials

--- event.go
+++ event.go
@@ -28,8 +28,8 @@ type Event struct {
 	ProviderType          string   `json:"_type"`
 	VPCID                 string   `json:"vpc_id"`
 	DatacenterRegion      string   `json:"datacenter_region"`
-	DatacenterAccessKey   string   `json:"datacenter_access_key"`
-	DatacenterAccessToken string   `json:"datacenter_access_token"`
+	DatacenterAccessKey   string   `json:"datacenter_secret"`
+	DatacenterAccessToken string   `json:"datacenter_token"`
 	NetworkAWSID          string   `json:"network_aws_id"`
 	SecurityGroupAWSIDs   []string `json:"security_group_aws_ids"`
 	InstanceAWSID         string   `json:"instance_aws_id,omitempty"`
